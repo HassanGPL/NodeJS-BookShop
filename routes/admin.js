688eb2afe4b77038d3a71b129e8ba760e34f9c4f
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-09-23 17:49:23 +0300

    Fitch Specific Product

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ router.get('/add-product', adminController.getAddProduct);
 
 router.post('/add-product', adminController.postAddProduct);
 
-// router.get('/edit-product/:productID', adminController.getEditProduct);
+router.get('/edit-product/:productID', adminController.getEditProduct);
 
 // router.post('/edit-product', adminController.postEditProduct);
 
