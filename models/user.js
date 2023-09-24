98b4f11c3bed063de351a79f38650bc12ae25f6c
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-09-24 14:29:33 +0300

    create user model and add a new user

--- models/user.js
+++ models/user.js
@@ -1,3 +1,36 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    cart: {
+        items: [{
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }]
+    }
+});
+
+
+module.exports = mongoose.model('User', userSchema);
+
+
+
 // const mongodb = require('mongodb');
 // const getDb = require('../helpers/databases').getDb;
 
