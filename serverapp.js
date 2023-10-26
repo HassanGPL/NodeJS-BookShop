ef648f0abde11555a9aff33e414c5ba05033e3dd
Hassan Ahmed <ahmedhassanmana@gmail.com>
2023-10-26 13:31:10 +0300

    handle serverapp error

--- serverapp.js
+++ serverapp.js
@@ -45,11 +45,14 @@ app.use((req, res, next) => {
     }
     User.findById(req.session.user._id)
         .then(user => {
+            if (!user) {
+                return next();
+            }
             req.user = user;
             next();
-        }
-        ).catch(err => {
-            console.log(err);
+        })
+        .catch(err => {
+            throw new Error(err);
         });
 
 });
